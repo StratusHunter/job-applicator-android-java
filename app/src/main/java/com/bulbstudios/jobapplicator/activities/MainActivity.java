85b6de229ea979328f5ac1191290054a7253d8d9
Terence Baker <terence@bulbstudios.com>
2019-04-01 08:25:20 +0100

    added res folders from kotlin project. matched build gradle to kotlin project

--- app/src/main/java/com/bulbstudios/jobapplicator/activities/MainActivity.java
+++ app/src/main/java/com/bulbstudios/jobapplicator/activities/MainActivity.java
@@ -1,17 +1,27 @@
 package com.bulbstudios.jobapplicator.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.bulbstudios.jobapplicator.R;
+import com.bulbstudios.jobapplicator.viewmodels.MainViewModel;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel viewModel = new MainViewModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setupButtonObserver() {
+
+        submitButton.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/bulbstudios/jobapplicator/viewmodels/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.bulbstudios.jobapplicator.viewmodels;
+
+/**
+ * Created by Terence Baker on 01/04/2019.
+ */
+public class MainViewModel {
+}
